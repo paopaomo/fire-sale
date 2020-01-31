a5b4019b7793f8ff13a67b49c2cde958720fc347
Zi Ye <18612319597@163.com>
2020-01-31 21:30:34 +0800

    [feature] set document title

--- app/renderer.js
+++ app/renderer.js
@@ -1,5 +1,6 @@
 const marked = require('marked');
 const { remote, ipcRenderer } = require('electron');
+const path = require('path');
 
 const mainProcess = remote.require('./main.js');
 
@@ -14,13 +15,29 @@ const showFileButton = document.getElementById('show-file');
 const openInDefaultButton = document.getElementById('open-in-default');
 
 const currentWindow = remote.getCurrentWindow();
+let filePath = '';
+let originalContent = '';
 
 const renderMarkdownToHtml = (markdown) => {
     htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
+const updateUserInterface = (isEdited) => {
+    let title = 'File Sale';
+    if(filePath) {
+        title = `${path.basename(filePath)}-${title}`;
+    }
+    if(isEdited) {
+        title = `${title} (Edited)`
+    }
+    currentWindow.setTitle(title);
+    currentWindow.setDocumentEdited(isEdited);
+};
+
 markdownView.addEventListener('keyup', (e) => {
-    renderMarkdownToHtml(e.target.value);
+    const currentContent = e.target.value;
+    renderMarkdownToHtml(currentContent);
+    updateUserInterface(currentContent !== originalContent);
 });
 
 openFileButton.addEventListener('click', () => {
@@ -32,6 +49,9 @@ newFileButton.addEventListener('click', () => {
 });
 
 ipcRenderer.on('file-opened', (event, file, content) => {
+    filePath = file;
+    originalContent = content;
     markdownView.innerHTML = content;
     renderMarkdownToHtml(content);
+    updateUserInterface();
 });
